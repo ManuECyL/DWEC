2fe4cd26289e69ce2ff54621713f9f73f081bfab
ManuECyL <manuel.chipri@educa.jcyl.es>
2023-09-27 20:23:54 +0200

    Tarea001

---
+++ Tema1_SintaxisJS/Tareas/Tarea001/js/funciones.js
@@ -0,0 +1,36 @@
+let intentos = 0;
+const min = 1;
+const max = 100;
+
+// Calculamos un número aleatorio entre un mínimo y un máximo establecido en las variables anteriores 
+let numeroAzar = Math.round(Math.random() * (max - min) + min);
+
+console.log("Número oculto " + numeroAzar);
+
+let numeroUsuario = prompt("Adivina el número entre " + min + " y " + max + " (o cancela para finalizar):");
+
+intentos ++;
+
+while (numeroUsuario != numeroAzar && numeroUsuario != null) {
+    
+    if (numeroUsuario > numeroAzar) {
+        alert("(" + intentos + ") es mayor que el número pedido")
+        numeroUsuario = prompt("Número entre " + min + " y " + max + " (o cancela para finalizar):");
+        intentos ++;
+    
+    } else if (numeroUsuario < numeroAzar) {
+        alert("(" + intentos + ") es menor que el número pedido")
+        numeroUsuario = prompt("Número entre " + min + " y " + max + " (o cancela para finalizar):");
+        intentos ++;
+    }
+}
+
+if (numeroUsuario == null) {
+    console.log("Número oculto: " + numeroAzar + "   Intentos: " + intentos);
+    alert("El usuario ha cancelado el juego con " + intentos + " intentos");
+}
+
+if (numeroUsuario == numeroAzar) {
+    console.log("Número acertado: " + numeroAzar + "   Intentos: " + intentos);
+    alert("Número acertado: " + numeroAzar + "   Intentos: " + intentos);
+}
