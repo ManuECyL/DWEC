c85411cb37ad2e7f347bcbf1f915a2ab6cb77385
ManuECyL <manuel.chipri@educa.jcyl.es>
2023-09-28 17:22:34 +0200

    For_Of

---
+++ Tema1_SintaxisJS/Pruebas/For_Of/js/funciones.js
@@ -0,0 +1,14 @@
+let datos = [5,23,12,85];
+let sumaDatos = 0;
+
+for (let valor of datos) {
+    sumaDatos += valor;
+}
+
+console.log(sumaDatos);
+
+let cadena = "Hola majos";
+
+for (let letra of cadena) {
+    console.log(letra);
+}
