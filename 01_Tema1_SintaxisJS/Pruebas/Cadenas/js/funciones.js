958ed3e1a0d103b70f05cd7120f200de0b922811
ManuECyL <manuel.chipri@educa.jcyl.es>
2023-10-04 20:15:09 +0200

    Cadenas

--- 01_Tema1_SintaxisJS/Pruebas/Cadenas/js/funciones.js
+++ 01_Tema1_SintaxisJS/Pruebas/Cadenas/js/funciones.js
@@ -1,1 +1,18 @@
 // MÉTODOS Y PROPIEDADES DE LAS CADENAS
+let cadena = "Hola";
+
+console.log(cadena);
+
+// Muestra la longitud de la cadena con .length
+console.log(cadena.length);
+
+// Devuelve la letra que se ocupa en la posición especificada con .charAt()
+console.log(cadena.charAt(0));
+
+// El método .split() se utiliza para separar cadenas de la forma especificada, devolviendo un array.
+alumno = "Manuel Chillón Prieto";
+
+console.log(alumno);
+console.log(alumno.split());
+console.log(alumno.split(" "));
+console.log(alumno[0].split());
