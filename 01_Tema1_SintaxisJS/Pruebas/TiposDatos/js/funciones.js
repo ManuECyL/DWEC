9bf52ea45654f3ba1770701d997be7ea433c3324
ManuECyL <manuel.chipri@educa.jcyl.es>
2023-10-04 19:19:36 +0200

    Tipos_de_datos

---
+++ 01_Tema1_SintaxisJS/Pruebas/TiposDatos/js/funciones.js
@@ -0,0 +1,18 @@
+// TIPOS DE DATOS
+console.log("probando que funciona...");
+console.log(typeof(3));
+console.log(typeof("3"));
+
+let x;
+console.log(x);
+console.log(typeof(null));
+console.log("Hola" * 2);
+console.log("3" * 2);
+console.log("3" + 2);
+console.log(3 + 2);
+
+let y = "3";
+console.log(y + 2);
+console.log(Number(y) + 2);
+console.log((-2 / 0));
+console.log(typeof(-2 / 0));
