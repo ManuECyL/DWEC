c3f42056444e177921bc6a5549998d247509dfd7
ManuECyL <manuel.chipri@educa.jcyl.es>
2023-10-07 13:48:38 +0200

    Tarea006

--- 01_Tema1_SintaxisJS/Tareas/Tarea006/js/funciones.js
+++ 01_Tema1_SintaxisJS/Tareas/Tarea006/js/funciones.js
@@ -1,7 +1,6 @@
-let divRombo = document.getElementById("divRombo");
-
-
 function mostrarRombo() {
+
+    let divRombo = document.getElementById("divRombo");
     let rombo = parseInt(document.getElementById("rombo").value);
 
     divRombo.innerHTML = "";
@@ -9,89 +8,75 @@ function mostrarRombo() {
     for (let i = 1; i <= (rombo / 2); i++) {
 
         for (let espacio = 1; espacio <= (rombo - i); espacio++) {
-            divRombo.innerHTML += "&nsbp;";
-            divRombo.innerHTML += "&nsbp;";
+            divRombo.innerHTML += "&nbsp;";
+            divRombo.innerHTML += "&nbsp;";
         }
 
-        for (let asteriscos = 1; asteriscos <= (i * 2); asteriscos++) {
+        for (let asteriscos = 1; asteriscos <= (i * 2) - 1; asteriscos++) {
             
             if (asteriscos == 1 || asteriscos == (i * 2) - 1) {
                 divRombo.innerHTML += "*";
             
             } else {
-                divRombo.innerHTML += "&nsbp;";
-                divRombo.innerHTML += "&nsbp;";
+                divRombo.innerHTML += "&nbsp;";
+                divRombo.innerHTML += "&nbsp;";
             }
         }
 
         divRombo.innerHTML += "<br></br>";
     }
-}
 
-// Si el número de filas es IMPAR
-if (rombo % 2 != 0) {
+    // Si el número de filas es IMPAR
+    if (rombo % 2 != 0) {
+        
+        for (let i = (rombo / 2) + 0.5; i >= 1; i--) {
     
-    for (let i = (rombo / 2) + 0.5; i >= i; i--) {
-
-        for (let espacio = 1; espacio <= array.length; espacio++) {
-            const element = array[espacio];
-            
+            for (let espacio = 1; espacio <= rombo - i; espacio++) {
+                divRombo.innerHTML += "&nbsp;";
+                divRombo.innerHTML += "&nbsp;";
+            }
+    
+            for (let asteriscos = 1; asteriscos <= ((i * 2) - 1); asteriscos++) {
+                
+                if (asteriscos == 1 || asteriscos == ((i * 2) - 1)) {
+                    divRombo.innerHTML += "*";
+                
+                } else {
+                    divRombo.innerHTML += "&nbsp;";
+                    divRombo.innerHTML += "&nbsp;";
+                }   
+            }
+    
+            divRombo.innerHTML += "<br></br>";
+        }
+    
+    // Si el número de filas es PAR
+    } else {
+    
+        for(let i = (rombo / 2); i >= 1; i--){
+    
+            for(let espacio = 1; espacio <= (rombo - i); espacio++){
+                divRombo.innerHTML += "&nbsp;";
+                divRombo.innerHTML += "&nbsp;";
+            }
+    
+            for(let asteriscos = 1; asteriscos <= ((i * 2) - 1); asteriscos++){
+    
+                if(asteriscos == 1 || asteriscos == ((i * 2) - 1)){
+                    divRombo.innerHTML += "*";
+    
+                } else {
+                    divRombo.innerHTML += "&nbsp;";
+                    divRombo.innerHTML += "&nbsp;";
+                }
+            }        
+    
+            divRombo.innerHTML += "<br></br>";
         }
     }
 }
-
 
 function limpiar() {
     document.getElementById("rombo").value = "";
     divRombo.innerHTML = "";
 }
-
-
-  // Si el número de filas es IMPAR
-//   if (filas % 2 != 0){
-
-//     for(let i = filas/2 + 0.5; i >= 1; i--){
-
-//         for(let blanco = 1; blanco <= filas - i; blanco++){
-//             mostrar.innerHTML += '&nbsp;';
-//             mostrar.innerHTML += '&nbsp;';
-//         }
-
-//         for(let astericos = 1; astericos <= (i*2)-1; astericos++){
-
-//             if(astericos == 1 || astericos == (i*2)-1){
-//                 mostrar.innerHTML += "*";
-
-//             } else {
-//                 mostrar.innerHTML += '&nbsp;';
-//                 mostrar.innerHTML += '&nbsp;';
-//             }
-//         }
-
-//         mostrar.innerHTML += "<br></br>";
-//     }
-
-// // Si el número de filas es PAR
-// } else {
-
-//     for(let i = filas/2; i >= 1; i--){
-
-//         for(let blanco = 1; blanco <= filas - i; blanco++){
-//             mostrar.innerHTML += '&nbsp;';
-//             mostrar.innerHTML += '&nbsp;';
-//         }
-
-//         for(let astericos = 1; astericos <= (i*2)-1; astericos++){
-
-//             if(astericos == 1 || astericos == (i*2)-1){
-//                 mostrar.innerHTML += "*";
-
-//             } else {
-//                 mostrar.innerHTML += '&nbsp;';
-//                 mostrar.innerHTML += '&nbsp;';
-//             }
-//         }        
-
-//         mostrar.innerHTML += "<br></br>";
-//     }
-// }
