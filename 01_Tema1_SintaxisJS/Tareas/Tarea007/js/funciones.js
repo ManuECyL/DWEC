6e322db061de17c00c250718f14eeab864f14560
ManuECyL <manuel.chipri@educa.jcyl.es>
2023-10-09 18:57:27 +0200

    Tarea007

---
+++ 01_Tema1_SintaxisJS/Tareas/Tarea007/js/funciones.js
@@ -0,0 +1,17 @@
+let filas = document.getElementById("filas").value;;
+let caracterL = document.getElementById("caracterL").value;;
+let caracterR = document.getElementById("caracterR").value;;
+let color = document.getElementById("color").value;
+
+// Rombo Hueco
+
+
+// Cuadrado Hueco
+for (let i = 1; i <= filas; i++) {
+    
+    for (let espacio = 0; espacio < array.length; espacio++) {
+        
+        
+    }
+    
+}
