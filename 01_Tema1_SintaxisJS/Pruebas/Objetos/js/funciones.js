0874e331719d3f6570c3977dce24b876d74dff9c
ManuECyL <manuel.chipri@educa.jcyl.es>
2023-10-16 18:00:22 +0200

    Objetos

--- 01_Tema1_SintaxisJS/Pruebas/Objetos/js/funciones.js
+++ 01_Tema1_SintaxisJS/Pruebas/Objetos/js/funciones.js
@@ -1,4 +1,4 @@
-let alumno = {
+let alumno1 = {
     nombre: "Georgi",
     apellido1: "Borisov",
     apellido2: "Alexandrov"
@@ -6,8 +6,50 @@ let alumno = {
 
 let nombreCompleto = "";
 
-for (let propiedad in alumno) {
-    nombreCompleto += alumno[propiedad] + " ";
+for (let propiedad in alumno1) {
+    nombreCompleto += alumno1[propiedad] + " ";
 }
 
 console.log(nombreCompleto);
+console.log("");
+
+/* FORMA ANTIGUA DE DECLARAR UN OBJETO Y SUS ATRIBUTOS
+
+    let alumno = new Object();
+
+    alumno.nombre = 'Carlos';
+    alumno[apellidos] = 'Pérez Órtiz';
+    alumno.edad = 19;
+*/
+
+
+// FORMA ACTUAL DE DECLARAR UN OBJETO Y SUS ATRIBUTOS
+
+    let alumno = {
+        nombre: 'Carlos',
+        apellidos: 'Pérez Órtiz',
+        edad: 19
+    };
+
+    let prop = 'apellidos';
+    prop = 'talla';
+
+    console.log(alumno[prop]);
+
+    console.log(alumno.nombre);
+    console.log(alumno['nombre']);
+    console.log(alumno[prop]);
+    
+    alumno.talla = {
+        max: 205
+    };
+
+// Comprueba primero si existe 'alumno.talla' al usar la '?' -> Si no existe saldrá 'undefined' en lugar de error
+    console.log(alumno.talla && alumno.talla.max);
+    console.log(alumno.talla?.max);
+    console.log(alumno);
+
+// Recorrer todas las propiedades de un objeto
+    for (let prop in alumno) {
+        console.log(prop + ": " + alumno[prop]);
+    }
