813316a86dea848da04cacf9d59a5b47f234fc05
ManuECyL <manuel.chipri@educa.jcyl.es>
2023-10-18 20:43:26 +0200

    Ejercicio 003 - Arrays

--- 02_Tema2_Arrays/Ejercicios/003/js/funciones.js
+++ 02_Tema2_Arrays/Ejercicios/003/js/funciones.js
@@ -1,8 +1,18 @@
 // Dado un array con los días de la semana obtén todos los días que empiezan por ‘M’
 
-let dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+let diasSemana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
 
-let diasM = dias.filter(dia => dia[0] == ("M"));
+// let diasM = diasSemana.filter(dia => dia[0] == ("M"));
 // let diasM = dias.filter(dia => dia.startsWith("M"));
+
+// console.log(diasM);
+
+let diasM = diasSemana.filter(function(dia, indice) {
+    
+    if (dia[0] == "M") {
+        console.log(indice + " : " + dia);
+        return true;
+    }
+});
 
 console.log(diasM);
