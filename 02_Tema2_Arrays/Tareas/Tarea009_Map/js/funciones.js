883cd5cdeb155c373e30060200973a9e276b197c
ManuECyL <manuel.chipri@educa.jcyl.es>
2023-10-19 17:15:53 +0200

    Tarea 009 - Map

---
+++ 02_Tema2_Arrays/Tareas/Tarea009_Map/js/funciones.js
@@ -0,0 +1,10 @@
+// Dado un array de notas, convertir todos los valores a número
+let notas = ['4', 5, '7', 0];
+
+console.log(notas);
+
+let arrayNumerico = notas.map(nota => Number(nota));
+
+console.log(arrayNumerico);
+
+
