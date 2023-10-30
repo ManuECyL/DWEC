11b099d610c10b1ec701d8d1e4cee65b09c308a2
ManuECyL <manuel.chipri@educa.jcyl.es>
2023-10-30 18:54:33 +0100

    Prototipos

---
+++ 03_Tema3_Objetos/Pruebas/Prototipos/js/prototipos.js
@@ -0,0 +1,17 @@
+console.log("Prototipo");
+ 
+Number.prototype.double = function () {
+    return this + this;
+}
+ 
+let numero = 3;
+console.log(numero.double());
+ 
+//---------------------------
+ 
+String.prototype.nombrePropio = function() {
+    return this.charAt(0).toUpperCase() + this.substring(1, this.length).toLowerCase();
+}
+ 
+let nombre = "mAnu";
+console.log(nombre.nombrePropio());
