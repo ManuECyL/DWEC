221c92a36deefd7ea2ffceef90e56b5af274c2e7
ManuECyL <manuel.chipri@educa.jcyl.es>
2023-10-31 23:03:45 +0100

    Tarea 014

--- 03_Tema3_Objetos/Tareas/Tarea011_ClasesAgregacion/js/clases/claseOrden.js
+++ 03_Tema3_Objetos/Tareas/Tarea011_ClasesAgregacion/js/clases/claseOrden.js
@@ -1,26 +1,47 @@
-
+import { Producto } from "./claseProducto.js";
 export class Orden {
 
     static contadorOrden = 0;
+    static MAX_PRODUCTOS = 5;
     arrayProductos = [];
 
     constructor() {
-        this.id = ++ Orden.contadorOrden;
+        this._id = ++ Orden.contadorOrden;
         this.arrayProductos = [];
+        Producto.contadorProductos = 0;
+    }
 
-        // this.total = total + precio; 
+    get idOrden() {
+        return this._id.toLocaleString(undefined,{minimumIntegerDigits: 3, useGrouping: false});;
     }
     
+// Método para añadir productos al ticket
     agregarProductos(producto) {
-        this.arrayProductos.push(producto);
+
+        if (Producto.contadorProductos >= Orden.MAX_PRODUCTOS) {
+            console.log("\nNo caben más productos en la Orden: " + this.idOrden);
+        
+        } else {
+            this.arrayProductos.push(producto);
+            Producto.contadorProductos ++;
+        }
+    }
+
+// Método para obtener el total del precio de todos los productos del ticket
+    calcularTotal() {
+        return this.arrayProductos.reduce((total, producto) => total += producto.precio,0)
     }
 
     toString() {
-    return `    ___________________
-    Orden: ${this.id.toLocaleString(undefined,{minimumIntegerDigits: 3, useGrouping: false})}
-        ${this.arrayProductos}
-    ---------------`;}
+        return `    __________________________________
+    Orden ${this.idOrden}:
+        - ${this.arrayProductos.reduce((todos,producto) => todos +=`\n\t\t- ${producto.toString()}`)}
+    - - - - - - - - - - - - - - - - -
+    Total: ${(this.calcularTotal()).moneda()}`;
+    }
 }
 
-
-// Total: ${this.total.toLocaleString('de-DE', {style: 'currency', currency: 'EUR'})}
+// Función para devolver el resultado de un precio en euros
+Number.prototype.moneda = function() {
+    return this.toLocaleString('de-DE', {style: 'currency', currency: 'EUR'});
+}
--- 03_Tema3_Objetos/Tareas/Tarea011_ClasesAgregacion/js/ventas.js
+++ 03_Tema3_Objetos/Tareas/Tarea011_ClasesAgregacion/js/ventas.js
@@ -6,11 +6,24 @@ let producto2 = new Producto('Americana', 214);
 let producto3 = new Producto('Pantalón', 160);
 
 let orden1 = new Orden();
-orden1.agregarProductos(producto1);
-orden1.agregarProductos(producto2);
-orden1.agregarProductos(producto3);
-
-
+    orden1.agregarProductos(producto1);
+    orden1.agregarProductos(producto2);
+    orden1.agregarProductos(producto3);
+    
 console.log(orden1.toString());
 
-// console.log(producto1);
+
+let producto4 = new Producto('pAntalón', 160);
+let producto5 = new Producto('americana', 214);
+
+let orden2 = new Orden();
+    orden2.agregarProductos(producto4);
+    orden2.agregarProductos(producto5);
+    orden2.agregarProductos(producto5);
+    orden2.agregarProductos(producto5);
+    orden2.agregarProductos(producto5);
+
+console.log(orden2.toString());
+
+orden2.agregarProductos(producto1);
+orden2.agregarProductos(producto1);
