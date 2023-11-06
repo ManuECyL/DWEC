822b4f931a26a765eb53f96818d3967cefaacdea
ManuECyL <manuel.chipri@educa.jcyl.es>
2023-11-06 17:50:17 +0100

    DOM - Creacion Nodos

--- 04_Tema4_DOM/Pruebas/DOM.js
+++ 04_Tema4_DOM/Pruebas/DOM.js
@@ -1,11 +1,31 @@
-console.log(document.childNodes);
+// Acceso a los nodos
+    console.log(document.childNodes);
 
-const nodosH = document.getElementsByTagName('h1');
-console.log(nodosH);
-console.log(nodosH[0]);
+    // Muestra los elementos 'h1' dentro del html
+    const nodosH = document.getElementsByTagName('h1');
+    console.log(nodosH);
+    console.log(nodosH[0]); // -> Colección con el primer elemento con etiqueta 'h1'
 
-const nodosP = nodosH[0].getElementsByTagName('p');
-console.log(nodosP);
-console.log(nodosP[0]);
+    // Muestra los elementos 'p' dentro del 'h1'
+    const nodosP = nodosH[0].getElementsByTagName('p');
+    console.log(nodosP);
+    console.log(nodosP[0]); // -> Colección con el primer elemento con etiqueta 'p'
 
-console.log(document.scripts);
+    // Muestra la colección de los scripts de la página
+    console.log(document.scripts);
+
+    console.log("");
+
+
+
+// Manipulación del DOM
+
+    // Añadir un elemento 'li' dentro de la lista 'ul'
+    const nuevoLi =  document.createElement('li');
+    const nuevoTexto = document.createTextNode("tercero");
+        nuevoLi.appendChild(nuevoTexto); // Añade el texto al elemento 'li'
+
+    // Añade el elemento 'li' al final de la lista 'ul'
+    // document.getElementsByTagName('ul')[0].appendChild(nuevoLi);
+
+    document.querySelector('ul').firstChild.before(nuevoLi); // Añade el elemento 'li' creado al principio de la lista 'ul'
