a5b6d0344f4d72c5ffc92c605a21a127e96d13f2
ManuECyL <manuel.chipri@educa.jcyl.es>
2023-11-06 18:00:39 +0100

    DOM_02

---
+++ 04_Tema4_DOM/Ejercicios/002_CreacionNodos/js/DOM_02.js
@@ -0,0 +1,13 @@
+let miDiv = document.getElementById('articulos'); 
+ 
+miDiv.innerHTML += '<p>Párrafo añadido al final</p>'; 
+ 
+let nuevoSegundoParrafo = document.createElement('p'); 
+nuevoSegundoParrafo.innerHTML = 'Este es el <strong>nuevo</strong> segundo párrafo'; 
+ 
+// let segundoParrafo = miDiv.children[1]; // Obtiene la posicion de el 2º párrafo del div 
+// miDiv.insertBefore(nuevoSegundoParrafo, segundoParrafo);
+
+
+const segundo = document.getElementById('articulos').getElementsByTagName('p');
+document.getElementById('articulos').insertBefore(nuevoSegundoParrafo, segundo);
