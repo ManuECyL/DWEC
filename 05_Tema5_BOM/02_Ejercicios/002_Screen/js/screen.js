5422ecec28f52d57e64ae779f23f4bc06f1ae0d4
ManuECyL <manuel.chipri@educa.jcyl.es>
2023-11-08 20:39:21 +0100

    Screen

---
+++ 05_Tema5_BOM/02_Ejercicios/002_Screen/js/screen.js
@@ -0,0 +1,21 @@
+let anchoPantalla = screen.width;
+let altoPantalla = screen.height;
+let anchoAvailPantalla = screen.availWidth;
+let altoAvailPantalla = screen.availHeight;
+
+let anchoVentana = window.innerWidth;
+let altoVentana = window.innerHeight;
+let anchoAvailVentana = window.outerWidth;
+let altoAvailVentana = window.outerHeight;
+
+console.log("Ancho Pantalla: " + anchoPantalla);
+console.log("Alto Pantalla: " + altoPantalla);
+console.log("Ancho Avail Pantalla: " + anchoAvailPantalla);
+console.log("Alto Avail Pantalla: " + altoAvailPantalla);
+
+console.log(" ");
+
+console.log("Ancho Ventana: " + anchoVentana);
+console.log("Alto Ventana: " + altoVentana);
+console.log("Ancho Avail Ventana: " + anchoAvailVentana);
+console.log("Alto Avail Ventana: " + altoAvailVentana);
