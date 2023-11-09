a3b45f3bbe0089f0bd2d99205b44ae619fe90643
ManuECyL <manuel.chipri@educa.jcyl.es>
2023-11-09 17:45:56 +0100

    Tarea 014

--- 04_Tema4_DOM/Tareas/Tarea014_Eventos/js/eventos.js
+++ 04_Tema4_DOM/Tareas/Tarea014_Eventos/js/eventos.js
@@ -3,6 +3,5 @@ const arrayTd = Array.from(document.getElementsByTagName("td"));
 arrayTd.forEach(td => {td.addEventListener('dblclick', cambiarColor)});
 
 function cambiarColor() {
-    
     this.style.backgroundColor = "green";
 }
