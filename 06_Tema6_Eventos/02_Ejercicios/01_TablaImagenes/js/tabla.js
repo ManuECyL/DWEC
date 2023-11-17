a3c96b4bfa7aec4a97e0957fbc77b911b4b8bca5
ManuECyL <manuel.chipri@educa.jcyl.es>
2023-11-17 18:14:37 +0100

    Tabla Imagenes

--- 06_Tema6_Eventos/02_Ejercicios/01_TablaImagenes/js/tabla.js
+++ 06_Tema6_Eventos/02_Ejercicios/01_TablaImagenes/js/tabla.js
@@ -1,2 +1,20 @@
+let arrayTd = Array.from(document.getElementsByTagName("td"));
+
+let arrayImagenes = new Array ();
+    arrayImagenes [0] = ['../imagenes/Img_1.png'];
+    arrayImagenes [1] = ['../imagenes/Img_2.png'];
+    arrayImagenes [2] = ['../imagenes/Img_3.png'];
+    arrayImagenes [3] = ['../imagenes/Img_4.png'];
+    arrayImagenes [4] = ['../imagenes/Img_5.png'];
+    arrayImagenes [5] = ['../imagenes/Img_6.png'];
 
 
+arrayTd.forEach(td => insertarImagen);
+
+
+function insertarImagen() {
+    
+    arrayImagenes.forEach(imagen => {
+        arrayTd.push(imagen);
+    });
+}
