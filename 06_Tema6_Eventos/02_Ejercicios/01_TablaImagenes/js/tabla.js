d6da82270c79e77e9fa0e42b140f9a8ea26e7fda
ManuECyL <manuel.chipri@educa.jcyl.es>
2023-11-18 18:39:22 +0100

    Tabla imagenes

--- 06_Tema6_Eventos/02_Ejercicios/01_TablaImagenes/js/tabla.js
+++ 06_Tema6_Eventos/02_Ejercicios/01_TablaImagenes/js/tabla.js
@@ -1,20 +1,21 @@
 let arrayTd = Array.from(document.getElementsByTagName("td"));
 
-let arrayImagenes = new Array ();
-    arrayImagenes [0] = ['../imagenes/Img_1.png'];
-    arrayImagenes [1] = ['../imagenes/Img_2.png'];
-    arrayImagenes [2] = ['../imagenes/Img_3.png'];
-    arrayImagenes [3] = ['../imagenes/Img_4.png'];
-    arrayImagenes [4] = ['../imagenes/Img_5.png'];
-    arrayImagenes [5] = ['../imagenes/Img_6.png'];
+let arrayImagenes = [
+    './imagenes/Img_1.png',
+    './imagenes/Img_2.png',
+    './imagenes/Img_3.png',
+    './imagenes/Img_4.png',
+    './imagenes/Img_5.png',
+    './imagenes/Img_6.png'
+];
+
+arrayTd.forEach((td, index) => insertarImagen(td, index));
 
 
-arrayTd.forEach(td => insertarImagen);
+function insertarImagen(td, index) {
 
-
-function insertarImagen() {
-    
-    arrayImagenes.forEach(imagen => {
-        arrayTd.push(imagen);
-    });
+    let img = document.createElement('img');
+        img.setAttribute('src', arrayImagenes[index])
+        
+    td.appendChild(img);
 }
