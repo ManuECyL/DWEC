192442a0b7384644485dc73cc4f3ff6c7bd3d21e
ManuECyL <manuel.chipri@educa.jcyl.es>
2023-11-23 16:49:09 +0100

    Ejercicio1

---
+++ 00_Examenes/01_1Ev/Ejercicio1/js/ejercicio1.js
@@ -0,0 +1,13 @@
+
+let bola = document.createElement('div');
+    bola.style.borderRadius = '50%';
+    bola.style.border = "2px solid";
+    bola.style.height = "100px";
+    bola.style.width = "100px";
+    bola.style.top = (window.innerHeight);
+    bola.style.left = (window.innerWidth);
+    bola.style.position = "absolute";
+    bola.moveT;
+
+
+document.body.appendChild(bola);
