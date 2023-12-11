9bdc9185b994b573da29fcf40c00c8a2c4e8b208
ManuECyL <manuel.chipri@educa.jcyl.es>
2023-12-11 14:29:13 +0100

    Tarea 22

--- 03_Tema3_Objetos/03_Tareas/Tarea022_JuegoDados/js/app.js
+++ 03_Tema3_Objetos/03_Tareas/Tarea022_JuegoDados/js/app.js
@@ -2,29 +2,112 @@ function numeroAleatorio(min, max) {
     return Math.floor((Math.random() * (max - min + 1)) + min);
 }
 
-let dineroRestante = 50;
-let restante = document.getElementById("restante");
 let apostado = document.getElementById("apostado");
+let restante = document.getElementById("restante");
+
+let dineroRestante = 50;
+restante.innerHTML = "Restante: " + dineroRestante + "€";
+
+function configurar() {
+    window.addEventListener("load", actualizarApuesta);
+}
+
 
 function actualizarApuesta() {
-    
-    let apuesta = document.getElementById("apuesta").value;
 
-    apostado.innerHTML = "Apostado: " + apuesta + "€";
-    console.log(apuesta);
+    let apuesta = parseInt(document.getElementById("apuesta").value);
+
+    if (apuesta > dineroRestante) {
+        alert("No puede apostar más de lo que tiene");
+
+    } else if (isNaN(apuesta)) {
+        apostado.innerHTML = "Apostado: 0€";
+
+    } else {
+        apostado.innerHTML = "Apostado: " + apuesta + "€";
+    }
 }
 
 
 function apostar() {
 
-    let resultado = document.getElementById("resultado");
+    let apuesta = parseInt(document.getElementById("apuesta").value);
 
-    let numeroApuesta = numeroAleatorio(1, 100);
+    if (apuesta > dineroRestante) {
+        alert("No puede apostar más de lo que tiene");
 
-    resultado.innerHTML = numeroApuesta;
+    } else if (isNaN(apuesta)) {
+        alert("Debe apostar algo");
 
-    let apuesta = document.getElementById("apuesta").value;
-    restante.innerHTML = "Restante: " + (dineroRestante - apuesta) + "€";
+    } else {
+
+        let resultado = document.getElementById("resultado");
+        let numeroApuesta = numeroAleatorio(1, 100);
+            resultado.innerHTML = numeroApuesta;
+
+        let numeroApuestaString = numeroApuesta.toString();
+        let numero1 = parseInt(numeroApuestaString[0]);
+        let numero2 = parseInt(numeroApuestaString[1] || 0); // Si no hay segundo número, se pone 0
+        
+        let mensaje = document.getElementById("mensaje");
+
+        let sumaNumeros = numero1 + numero2;
+
+        switch (sumaNumeros) {
+            
+            case 7:
+                mensaje.style.backgroundColor = "green";
+                mensaje.innerHTML = "7: Has ganado " + apuesta + "€";
+                dineroRestante += apuesta;
+                break;
+        
+            case 11:
+                mensaje.style.backgroundColor = "green";
+                mensaje.innerHTML = "11: Has ganado " + apuesta + "€";
+                dineroRestante += apuesta;
+                break;
+
+            case 2:
+                mensaje.style.backgroundColor = "red";
+                mensaje.innerHTML = "2: Has perdido " + apuesta * 2 + "€";
+                dineroRestante -= apuesta * 2;           
+                
+                if (dineroRestante == 0 || dineroRestante < 0) {
+                    mensaje.innerHTML += "<br> HAS PERDIDO";
+                }
+
+                break;
+            
+            case 3:
+                mensaje.style.backgroundColor = "red";
+                mensaje.innerHTML = "3: Has perdido " + apuesta * 2 + "€";
+                dineroRestante -= apuesta * 2;    
+                
+                if (dineroRestante == 0 || dineroRestante < 0) {
+                    mensaje.innerHTML += "<br> HAS PERDIDO";
+                }
+
+                break;
+
+            case 12:
+                mensaje.style.backgroundColor = "red";
+                mensaje.innerHTML = "12: Has perdido " + apuesta * 2 + "€";
+                dineroRestante -= apuesta * 2;
+                
+                if (dineroRestante == 0 || dineroRestante < 0) {
+                    mensaje.innerHTML += "<br> HAS PERDIDO";
+                }
+
+                break;
+                
+            default:
+                mensaje.style.backgroundColor = null;
+                mensaje.innerHTML = sumaNumeros + ": Vuelve a lanzar";
+                break;
+        }
+
+        restante.innerHTML = "Restante: " + dineroRestante + "€";
+    }
 }
 
 
