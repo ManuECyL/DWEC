f43597b378462665a0e2e76808416a0710fd194f
ManuECyL <manuel.chipri@educa.jcyl.es>
2024-01-18 18:01:54 +0100

    API_01

--- 09_Tema9_JSON/API_01/controllers/ciudadesController.js
+++ 09_Tema9_JSON/API_01/controllers/ciudadesController.js
@@ -39,8 +39,120 @@ const postCiudades = (req, res) => {
     });
 };
 
+// Función para obtener los datos de una ciudad según el id indicado
+const getCiudadesById = (req, res) => {
+
+    const idRegistro = req.params.id;
+
+    // Consulta a la base de datos
+    db.query('SELECT * FROM ciudades WHERE id = ?', [idRegistro], (err, resultados) => { // http://localhost:3000/ciudades/3
+
+        if (err) {
+            console.error('Error de conexión a la base de datos: ', err);
+            res.status(500).json({error: 'Error interno del servidor'})
+    
+        } else {
+
+            // Verifica si se encontró un registro
+            if (resultados.length > 0) {
+                // Devuelve el primer resultado encontrado (debería ser único)
+                res.json(resultados[0]);
+
+            } else {
+                res.status(400).json({error : 'Registro no encontrado'});
+            }
+        }
+
+    });
+};
+
+// Función para modificar los datos de una ciudad según el id indicado
+const putCiudades = (req, res) => {
+
+    const idRegistro = req.params.id;
+    const {nuevoNombre, habitantes} = req.body;
+    const sql = 'UPDATE ciudades SET nombre = ?, cantidad = ? WHERE id = ?';
+
+    db.query(sql, [nuevoNombre, habitantes, idRegistro], (err, resultados) => { // http://localhost:3000/ciudades/put/3
+
+        if (err) {
+            console.error('Error al insertar datos en la base de datos: ', err);
+            res.status(500).json({error: 'Error interno del servidor'})
+    
+        } else {
+            res.json({recibido: true, nuevoNombre, habitantes, id:resultados.idRegistro});
+        }
+
+    });
+}
+
+// Función para modificar de otra forma los datos de una ciudad según el id indicado
+const patchCiudades = (req, res) => {
+
+    const idRegistro = req.params.id;
+    const {nuevoNombre, habitantes} = req.body;
+    const sql = 'UPDATE ciudades SET nombre = ?, cantidad = ? WHERE id = ?';
+
+    db.query(sql, [nuevoNombre, habitantes, idRegistro], (err, resultados) => { // http://localhost:3000/ciudades/patch/3
+
+        if (err) {
+            console.error('Error al insertar datos en la base de datos: ', err);
+            res.status(500).json({error: 'Error interno del servidor'})
+    
+        } else {
+            res.json({recibido: true, nuevoNombre, habitantes, id:resultados.idRegistro});
+        }
+
+    });
+}
+
+// Función para actualizar de forma natural los datos de una ciudad según el id indicado
+const actualizarCiudades = (req, res) => {
+
+    const idRegistro = req.params.id;
+    const {nuevoNombre, habitantes} = req.body;
+    
+    // Array donde van a ir los nombres de los campos que se van a actualizar
+    const updatedFields = [];
+
+    // Array donde van a ir los valores de los campos que se van a actualizar
+    const updatedValues = [];
+
+    // Si nuevoNombre no es undefined, añadir el campo y valor a los arrays
+    if (nuevoNombre != undefined) {
+        updatedFields.push('nombre=?');
+        updatedValues.push(nuevoNombre);
+    }
+
+    // Si habitantes no es undefined, añadir el campo y valor a los arrays
+    if (nuevoNombre != undefined) {
+        updatedFields.push('cantidad=?');
+        updatedValues.push(habitantes);
+    }
+
+    const sql = `UPDATE ciudades SET ${updatedFields.join(', ')} WHERE id = ?`;
+    const queryValues = [...updatedValues, idRegistro];
+
+    db.query(sql, queryValues, (err, resultados) => { // http://localhost:3000/ciudades/3
+
+        if (err) {
+            console.error('Error al insertar datos en la base de datos: ', err);
+            res.status(500).json({error: 'Error interno del servidor'})
+    
+        } else {
+            res.json({recibido: true, nuevoNombre, habitantes, id:resultados.idRegistro});
+        }
+
+    });
+}
+
+
 // Exportaciones que vamos a realizar
 module.exports = {
     getCiudades,
-    postCiudades
+    postCiudades,
+    getCiudadesById,
+    putCiudades,
+    patchCiudades,
+    actualizarCiudades
 };
--- 09_Tema9_JSON/API_01/rutas/rutas_ciudades.js
+++ 09_Tema9_JSON/API_01/rutas/rutas_ciudades.js
@@ -13,5 +13,17 @@ rutasCiudades.get('/', ciudadesController.getCiudades);
 // POST de rutasCiudades
 rutasCiudades.post('/add', ciudadesController.postCiudades);
 
+// GET de rutasCiudades por Id
+rutasCiudades.get('/:id', ciudadesController.getCiudadesById);
+
+// PUT de rutasCiudades por Id (Modificar)
+rutasCiudades.put('/put/:id', ciudadesController.putCiudades);
+
+// PATCH de rutasCiudades por Id (Modificar de otra forma) 
+rutasCiudades.patch('/patch/:id', ciudadesController.patchCiudades);
+
+// PATCH de rutasCiudades por Id (Modificar de forma natural) 
+rutasCiudades.patch('/:id', ciudadesController.actualizarCiudades);
+
 // Exportamos la constante routes con la colección de rutasCiudades
 module.exports = rutasCiudades;
