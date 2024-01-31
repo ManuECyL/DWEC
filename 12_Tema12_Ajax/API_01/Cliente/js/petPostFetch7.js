9216063862c5c56c60498a0ae737287d175235f4
ManuECyL <manuel.chipri@educa.jcyl.es>
2024-01-31 20:45:06 +0100

    Promesas y Fetch

--- 12_Tema12_Ajax/API_01/Cliente/js/petPostFetch7.js
+++ 12_Tema12_Ajax/API_01/Cliente/js/petPostFetch7.js
@@ -1,5 +1,5 @@
 // POST con la API Fetch() 
-const url = `http://${dirIP_api}:${PUERTO_EXPRESS}`;
+// const url = `http://${dirIP_api}:${PUERTO_EXPRESS}`;
 
 document.getElementById('addCiudad').addEventListener('submit', (event)=>{
 
@@ -11,10 +11,13 @@ document.getElementById('addCiudad').addEventListener('submit', (event)=>{
     }
 
     fetch(url + '/ciudades/add' , {
-        method: 'POST' // o PUT o PATCH
-    })
+        method: 'POST', // o PUT o PATCH o DELETE...
+        body: JSON.stringify(datosBody), // Pasamos a cadena datosBody
+        headers: {
+            'Content-Type': 'application/json'
+        }
 
-        .then(datosCrudos => {
+    }).then(datosCrudos => {
 
             // Si la respuesta no es correcta, lanzamos una excepción que intercepta el catch
             if (!datosCrudos.ok) {
@@ -28,7 +31,7 @@ document.getElementById('addCiudad').addEventListener('submit', (event)=>{
 
         .then(datosObjeto => {
             console.log(datosObjeto);
-            document.getElementById('p1').innerHTML = datosObjeto.nombre + ": " + datosObjeto.cantidad;
+            document.getElementById('p1').innerHTML = "Ciudad añadida: " + datosObjeto.nombre + ": " + datosObjeto.cantidad;
         })
 
         .catch(error => console.error(error));
