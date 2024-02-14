f50bfd3a94e8985708b6ef8a9bf96fc3e9d9b9b9
ManuECyL <manuel.chipri@educa.jcyl.es>
2024-02-14 19:08:27 +0100

    Ejercicio 1 Bind

--- 00_Examenes/03_2Ev/Ejercicio1/js/app_bind.js
+++ 00_Examenes/03_2Ev/Ejercicio1/js/app_bind.js
@@ -1,25 +1,60 @@
+// function calcular(operacion, x, y) { 
+//     switch (operacion) { 
+//       case 'suma': 
+//         return x + y; 
+//       case 'resta': 
+//         return x - y; 
+//       case 'multiplicacion': 
+//         return x * y; 
+//       case 'division': 
+//         return x / y; 
+//       default: 
+//         return NaN; 
+//     } 
+// }
+
+
+// function duplicar(num) {
+
+//     num = calcular.bind(null,'multiplicacion', num, 2); 
+
+//     return num();
+// }
+
+// console.log(duplicar(3));
+// console.log(duplicar(5));
+
+
 function calcular(operacion, x, y) { 
-    switch (operacion) { 
-      case 'suma': 
-        return x + y; 
-      case 'resta': 
-        return x - y; 
-      case 'multiplicacion': 
-        return x * y; 
-      case 'division': 
-        return x / y; 
-      default: 
-        return NaN; 
-    } 
+  switch (operacion) { 
+    case 'suma': 
+      return x + y; 
+    case 'resta': 
+      return x - y; 
+    case 'multiplicacion': 
+      return x * y; 
+    case 'division': 
+      return x / y; 
+    default: 
+      return NaN; 
+  } 
 }
 
 
 function duplicar(num) {
 
-    num = calcular.bind(null,'multiplicacion', num, 2); 
+  num = calcular('multiplicacion', num, 2); 
 
-    return num();
+  return num;
 }
 
-console.log(duplicar(3));
-console.log(duplicar(5));
+
+function bindDuplicar(valor) {
+
+  const num = duplicar.bind(null, valor);
+
+  return num();
+}
+
+console.log(bindDuplicar(3));
+console.log(bindDuplicar(5));
